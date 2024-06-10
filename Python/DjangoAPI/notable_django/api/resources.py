c43d9e0b028d539577bd1527a812572bbd782f07
KimuchuJr <kimuchukim@gmail.com>
2024-06-10 20:17:19 +0300

    Authorization Request

--- Python/DjangoAPI/notable_django/api/resources.py
+++ Python/DjangoAPI/notable_django/api/resources.py
@@ -1,7 +1,9 @@
 from tastypie.resources import ModelResource
 from api.models import Note
+from tastypie.aithorization import Autorization
 
 class NoteResource(ModelResource):
     class Meta:
         queryset = Note.objects.all()
         resource_name = 'note'
+        authorization = Authorization()
